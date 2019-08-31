363332c01ea2b9f2b3049d70cfdcac2fa654ccc5
admin <admin>
2019-08-31 22:23:29 +0200

    Edited file 'js/form.js'

--- js/form.js
+++ js/form.js
@@ -1,11 +1,13 @@
 var app = angular.module('myplugin.module', []);
 
 app.controller('MyCustomFormController', function($scope) {
+     console.log("start")
     var updateChoices = function() {
         // the parameter to callPythonDo() is passed to the do() method as the payload
         // the return value of the do() method comes back as the data parameter of the fist function()
         $scope.callPythonDo({}).then(function(data) {
             // success
+             console.log("data")
             console.log(data)
             $scope.choices = data.choices;
         }, function(data) {
