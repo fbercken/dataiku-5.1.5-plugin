ffe3ce1ed8fd7d0ea463daffb581fdabe79857c9
admin <admin>
2019-09-01 20:17:53 +0200

    Edited file 'python-runnables/my-macro/runnable.py'

---
+++ python-runnables/my-macro/runnable.py
@@ -0,0 +1,16 @@
+from dataiku.runnables import Runnable
+
+class MyRunnable(Runnable):
+    def __init__(self, project_key, config, plugin_config):
+        self.project_key = project_key
+        self.config = config
+        self.plugin_config = plugin_config
+        
+    def get_progress_target(self):
+        return None
+
+    def run(self, progress_callback):
+        
+        
+        return '<div>The values in the form are:</div><pre class="debug">%s</pre>' % self.config
+        
