be2107992a349604b2a3add357059ad15074e741
admin <admin>
2019-09-01 02:42:43 +0200

    Edited file 'resource/form1.py'

--- resource/form1.py
+++ resource/form1.py
@@ -1,11 +1,10 @@
 from blueRest import BlueData
 
 def do(payload, config, plugin_config, inputs):
-    choices = ['aggg', 'b', 'c'] # fetch it however you like.
     
     restClient = BlueData( { "hostname": "35.177.158.117", "user": "admin", "password": "admin123" } )
     restClient.connect()
     
-    choices = [ restClient.headers['X-BDS-SESSION'] ]
+    tenants = [ restClient.headers['X-BDS-SESSION'] ]
     
-    return {'choices' : choices}
+    return {'tenants' : tenants}
