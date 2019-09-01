f3011077c6eac838f51e6f9e16fd75a6030c3ba9
admin <admin>
2019-09-01 14:25:15 +0200

    Edited file 'resource/form1.py'

--- resource/form1.py
+++ resource/form1.py
@@ -2,7 +2,26 @@ from blueRest import BlueData
 
 restClient = {}
 
+def connect(config,):
+    restClient = BlueData( { "hostname": config["hostname"], "user": config["username"], "password": config["password"] } )
+    restClient.connect()
+    tenants = restClient.getTenants()
+    return {'tenants': tenants }
+  
+def applications(config):
+    #restClient.setTenant( "/api/v2/tenant/2", "Site Admin")
+    #applications = restClient.getApplications()
+    return { "applications": applications }
+
+actions = {
+    "connect": connect,
+    "applications": applications
+}
+
+
 def do(payload, config, plugin_config, inputs):
+    
+    return actions[payload["method"]](config)
     
     if ( payload["method"] == "connect"):
         restClient = BlueData( { "hostname": config["hostname"], "user": config["username"], "password": config["password"] } )
