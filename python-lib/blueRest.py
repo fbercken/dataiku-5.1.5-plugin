dd6dd4213d8e6a1eaab2288b7ec20c3aaa37c7be
admin <admin>
2019-09-01 19:20:34 +0200

    Edited file 'python-lib/blueRest.py'

--- python-lib/blueRest.py
+++ python-lib/blueRest.py
@@ -33,6 +33,8 @@ class BlueData(object):
         self.user = { "name":  config['username'], "password": config['password'] }
         self.session = { "password": config['password'], "tenant": "/api/v2/tenant/1", "role": "/api/v1/role/1", "site_admin_view": True }
         self.headers =  { "Accept": "applicaiton/json", "Content-type": "application/json" }
+        if config['sessionid']:
+            self.headers["X-BDS-SESSION"] = config['sessionid']
 
 
     def connect(self):
