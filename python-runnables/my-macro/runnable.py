dc631f2fb8b95d1be52f3e5f9ff6d8c4688663bc
admin <admin>
2019-09-01 20:32:19 +0200

    Edited file 'python-runnables/my-macro/runnable.py'

--- python-runnables/my-macro/runnable.py
+++ python-runnables/my-macro/runnable.py
@@ -11,7 +11,7 @@ class MyRunnable(Runnable):
         return None
 
     def run(self, progress_callback):
-        restClient = BlueData(config)
+        restClient = BlueData(self.config)
         restClient.connect()
         
         
