476d790684192cec88df016d1c539387e6beffa5
admin <admin>
2019-09-01 18:25:15 +0200

    Edited file 'resource/form1.py'

--- resource/form1.py
+++ resource/form1.py
@@ -6,7 +6,7 @@ from blueRest import BlueData
 
 
 def connect(config):
-    restClient = BlueData( { "hostname": config["hostname"], "user": config["username"], "password": config["password"] } )
+    restClient = BlueData( { "hostname": config["hostname"], "username": config["username"], "password": config["password"] } )
     restClient.connect()
     tenants = restClient.getTenants()
     sessionid = restClient.getSessionId()
