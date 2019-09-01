82f07fcf3c8973ed7b4785ad0c6d68652a72caf6
admin <admin>
2019-09-01 11:41:55 +0200

    Edited file 'resource/form1.py'

--- resource/form1.py
+++ resource/form1.py
@@ -5,7 +5,6 @@ def do(payload, config, plugin_config, inputs):
    # if ( payload["method"] == "connect"):
        # restClient = BlueData( { "hostname": plugin_config.hostname, "user": plugin_config.username, "password": plugin_config.password } )
        # restClient.connect()
-        tenants = [] 
-        #[ restClient.headers['X-BDS-SESSION'] ]
-    
+       # tenants = [ restClient.headers['X-BDS-SESSION'] ]
+    tenants = [] 
     return {'tenants' : tenants, "method": payload, "config": config, "plugin_config": plugin_config, "inputs": inputs }
