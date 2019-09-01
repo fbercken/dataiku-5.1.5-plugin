a440a913476ed53b23c851197f181702604af495
admin <admin>
2019-09-01 16:09:28 +0200

    Edited file 'resource/form1.py'

--- resource/form1.py
+++ resource/form1.py
@@ -25,9 +25,4 @@ def do(payload, config, plugin_config, inputs):
     
     return actions[payload["method"]](config)
     
-   # if ( payload["method"] == "connect"):
-   #     restClient = BlueData( { "hostname": config["hostname"], "user": config["username"], "password": config["password"] } )
-    #    restClient.connect()
-    #    tenants = restClient.getTenants()
-
     #return {'tenants': tenants, "method": payload, "config": config, "plugin_config": plugin_config, "inputs": inputs }
