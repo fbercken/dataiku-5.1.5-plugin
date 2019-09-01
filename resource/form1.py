d266a5680357c601911c3b96ea287cd7bb65f9e9
admin <admin>
2019-09-01 11:35:17 +0200

    Edited file 'resource/form1.py'

--- resource/form1.py
+++ resource/form1.py
@@ -3,7 +3,7 @@ from blueRest import BlueData
 def do(payload, config, plugin_config, inputs):
     
     if ( payload["method"] == "connect"):
-        restClient = BlueData( { "hostname": config.hostname, "user": config.username, "password": config.password } )
+        restClient = BlueData( { "hostname": plugin_config.hostname, "user": plugin_config.username, "password": plugin_config.password } )
         restClient.connect()
         tenants = [ restClient.headers['X-BDS-SESSION'] ]
     
