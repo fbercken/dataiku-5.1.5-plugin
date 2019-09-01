57dd5e286f58306c7e6b7edf4c7bf4caedc314e5
admin <admin>
2019-09-01 14:28:12 +0200

    Edited file 'resource/form1.py'

--- resource/form1.py
+++ resource/form1.py
@@ -27,9 +27,5 @@ def do(payload, config, plugin_config, inputs):
         restClient = BlueData( { "hostname": config["hostname"], "user": config["username"], "password": config["password"] } )
         restClient.connect()
         tenants = restClient.getTenants()
-        #[ restClient.headers['X-BDS-SESSION'] ]
-        
-        
-        #restClient.setTenant( "/api/v2/tenant/2", "Site Admin")
 
     return {'tenants': tenants, "method": payload, "config": config, "plugin_config": plugin_config, "inputs": inputs }
