c259fc3c7a725a6dd81ee25a5dc817edd13cd01b
admin <admin>
2019-09-09 10:41:09 +0200

    Edited file 'python-runnables/create-cluster/runnable.py'

--- python-runnables/create-cluster/runnable.py
+++ python-runnables/create-cluster/runnable.py
@@ -5,8 +5,8 @@ import json
 
 class MyRunnable(Runnable):
     def __init__(self, project_key, config, plugin_config):
-        self.project_key = project_key
         self.config = config
+        self.project_key = project_key
         self.plugin_config = plugin_config
         
     def get_progress_target(self):
