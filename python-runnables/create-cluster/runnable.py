5f827165731d0a59d4d31532f12e17001b969fd7
admin <admin>
2019-09-09 09:40:59 +0200

    Edited file 'python-runnables/create-cluster/runnable.py'

--- python-runnables/create-cluster/runnable.py
+++ python-runnables/create-cluster/runnable.py
@@ -45,4 +45,4 @@ class MyRunnable(Runnable):
 
         #return '<div>The values in the form are:</div><pre class="debug">%s</pre>' % self.config % restClient.getTenants()
         
-        return '<div>The Cluster has been created sucessfully:</div><pre class="debug">%s</pre>' % data1 
+        return '<div>The Cluster has been created sucessfully:</div><pre class="debug">%s</pre>' % data 
