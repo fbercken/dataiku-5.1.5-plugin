3a07d8237927f2a59a983dcb62f272ea4e2d091a
admin <admin>
2019-09-09 10:50:45 +0200

    Edited file 'python-runnables/create-cluster/runnable.py'

--- python-runnables/create-cluster/runnable.py
+++ python-runnables/create-cluster/runnable.py
@@ -43,7 +43,7 @@ class MyRunnable(Runnable):
         
         data1 = json.dumps(clusterSpec)
         
-        data = restClient.createCluster(zz)
+        data = restClient.createCluster(clusterSpec)
 
         #return '<div>The values in the form are:</div><pre class="debug">%s</pre>' % self.config % restClient.getTenants()
         
